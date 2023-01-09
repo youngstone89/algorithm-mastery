f54e44fb20faa868f207d4bd20d50bad238b50b3
youngstone89 <youngstone89@icloud.com>
2023-01-09 22:05:16 +0900

    romanToInteger

---
+++ hashmap/java/romanToInteger.java
@@ -0,0 +1,33 @@
+import java.util.HashMap;
+import java.util.Map;
+
+class RomanToInteger {
+
+    public int romanToInt(String s) {
+        Map<Character, Integer> romanMap = new HashMap<>();
+        romanMap.put('I', 1);
+        romanMap.put('V', 5);
+        romanMap.put('X', 10);
+        romanMap.put('L', 50);
+        romanMap.put('C', 100);
+        romanMap.put('D', 500);
+        romanMap.put('M', 1000);
+        int n = s.length();
+        int num = romanMap.get(s.charAt(n - 1));
+        for (int i = n - 2; i >= 0; i--) {
+            if (romanMap.get(s.charAt(i)) >= romanMap.get(s.charAt(i + 1))) {
+                num += romanMap.get(s.charAt(i));
+            } else {
+                num -= romanMap.get(s.charAt(i));
+            }
+        }
+        return num;
+    }
+    public static void main(String args[]) {
+        RomanToInteger rti = new RomanToInteger();
+        System.out.println(rti.romanToInt("III"));
+        System.out.println(rti.romanToInt("LVIII"));
+        System.out.println(rti.romanToInt("MCMXCIV"));
+        System.out.println(rti.romanToInt("VIV"));
+    }
+}

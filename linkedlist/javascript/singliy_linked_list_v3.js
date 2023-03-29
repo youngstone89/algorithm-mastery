e8876d679f16a74a0986e74dba3d938d274f8138
youngstone89 <youngstone89@icloud.com>
2023-03-30 07:19:56 +0900

    removeFromBack, removeFromFront

--- linkedlist/javascript/singliy_linked_list_v3.js
+++ linkedlist/javascript/singliy_linked_list_v3.js
@@ -12,7 +12,35 @@ class LinkedList {
         this.tail = null;
         this.size = 0;
     }
+    removeFromFront() {
+        if (this.head === null) { 
+            return
+        }
+        this.head = this.head.next
+        this.size -= 1
+    }
+    removeFromBack() {
+        if (this.tail === null) {
+            return
+        }
+        let prevNode = null
+        let current = this.head
+        while (current.next !== null) { 
+            prevNode = current
+            current = current.next
+        }
+        // set prev node to null
+        if (this.size > 1) { 
+            prevNode.next = null
+            this.tail = prevNode
+        } else {
+            this.head = null
+            this.tail = null
+        }
+        this.size -= 1
+    }
     addToFront(data) {
+        this.size+=1
         const node = new Node(data)
         if (this.head === null) {
             this.head = node
@@ -25,7 +53,8 @@ class LinkedList {
         this.head = node
 
     }
-	addToBack(data) {
+    addToBack(data) {
+        this.size+=1
 		// handle edge cases
 		// edge case 1: list is empty, set the new node to the head and the tail reference
         // because we can't verify if the next reference is null or not in empty list
@@ -52,11 +81,10 @@ class LinkedList {
         console.log('head: ', this.head)
         let current = this.head
         let str = ''
-        while (current.next !== null) {
+        while (current !== null) {
             str += current.data + ' '
             current = current.next
         }
-        str += current.data + ' '
         console.log(str)
         console.log('tail: ', this.tail)
     }
@@ -66,5 +94,12 @@ let list = new LinkedList();
 list.addToFront(0)
 list.addToFront(1)
 list.addToFront(2)
+list.removeFromFront()
+list.removeFromBack()
+list.removeFromBack()
+list.addToBack(0)
+list.addToBack(1)
+list.addToFront(3)
+list.removeFromFront()
 list.printList()
 

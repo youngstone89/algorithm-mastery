07d18398a2a5f0cfcc8603a4d25324a13018c963
youngstone89 <youngstone89@icloud.com>
2023-04-20 13:51:21 +0900

    reverse string without changing special chars

--- string/reverse_string.js
+++ string/reverse_string.js
@@ -1,9 +1,43 @@
-let s = "Ab,c,de!$";
+// Javascript program to reverse string without
+// affecting it's special character
 
-let temp = new Array(s.length);
-console.log(temp);
+function reverse(s)
+{
+	// creating character array of size
+	// equal to length of string
+	let temp=new Array(s.length);
+    let x = 0;
+    // iterate through s and store alphabetic characters
+    for (let i = 0; i < s.length; i++){
+        if (s[i] >= 'a' && s[i] <= 'z' || s[i] >= 'A' && s[i] <= 'Z') {
+            temp[x] = s[i];
+            x += 1;
+        }
+    }
+	
+	// reversing the character array
+	//reverse(temp, temp + x);
+	let str="";
+	for(let i=x-1; i>=0; i--)
+	{
+		str+=temp[i];
+	}
 
-console.log('A' >= 'a' && 'A' <= 'z')
-console.log('a' >= 'a' && 'a' <= 'z')
-console.log('A' >= 'A' && 'A' <= 'Z')
-console.log(',' >= 'A'&& ',' <= 'Z')
+	x = 0;
+	let ans=""
+	for (let i = 0; i < s.length; i++) {
+		if (s[i] >= 'a' && s[i] <= 'z'
+			|| s[i] >= 'A' && s[i] <= 'Z') {
+			// updating the original string
+			ans+= str[x];
+			x++;
+		}
+		else
+			ans+=s[i];
+	}
+	console.log("reversed string is: " + ans);
+}
+// driver code
+	let s = "Ab,c,de!$";
+	// function call
+	reverse(s);

c9974da5cfe88dd196f2faf0ca2a2d0200de651e
youngstone89 <youngstone89@icloud.com>
2023-04-20 11:55:30 +0900

    reverse algorithm

---
+++ string/reverse_string.js
@@ -0,0 +1,9 @@
+let s = "Ab,c,de!$";
+
+let temp = new Array(s.length);
+console.log(temp);
+
+console.log('A' >= 'a' && 'A' <= 'z')
+console.log('a' >= 'a' && 'a' <= 'z')
+console.log('A' >= 'A' && 'A' <= 'Z')
+console.log(',' >= 'A'&& ',' <= 'Z')

88ba58ef6060ad9f073e1f7f5bceb3dc43e3f049
youngstone89 <youngstone89@icloud.com>
2023-09-07 06:49:11 +0900

    network delay time dijkstra

---
+++ graph/java/NetworkDelayTimeDijkstra.java
@@ -0,0 +1,53 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+public class NetworkDelayTimeDijkstra {
+
+    public static int networkDelayTime(int[][] times, int N, int K) {
+        Map<Integer, Map<Integer, Integer>> map = new HashMap<>();
+        for (int[] time : times) {
+            map.putIfAbsent(time[0], new HashMap<>());
+            map.get(time[0]).put(time[1], time[2]);
+        }
+
+        // distance, node into pq
+        Queue<int[]> pq = new PriorityQueue<>((a, b) -> (a[0] - b[0]));
+
+        pq.add(new int[] { 0, K });
+
+        boolean[] visited = new boolean[N + 1];
+        int res = 0;
+
+        while (!pq.isEmpty()) {
+            int[] cur = pq.remove();
+            int curNode = cur[1];
+            int curDist = cur[0];
+            if (visited[curNode])
+                continue;
+            visited[curNode] = true;
+            res = curDist;
+            N--;
+            if (map.containsKey(curNode)) {
+                for (int next : map.get(curNode).keySet()) {
+                    pq.add(new int[] { curDist + map.get(curNode).get(next), next });
+                }
+            }
+        }
+        return N == 0 ? res : -1;
+
+    }
+
+    public static void main(String[] args) {
+
+        int[][] times = new int[][] {
+                { 2, 1, 1 },
+                { 2, 3, 1 },
+                { 3, 4, 1 },
+        };
+        System.out.println(networkDelayTime(times, 4, 2));
+    }
+}
+
+// Time Limit Exceeded
